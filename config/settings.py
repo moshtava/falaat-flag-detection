9320f97f660c4cce009f5a393c767dd5cf2b7039
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2025-01-21 13:45:45 +0330

    chore: add Channel layers configuration (using Redis)

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 ASGI_APPLICATION = 'config.asgi.application'
+
+CHANNEL_LAYERS = {
+'default': {
+'BACKEND': 'channels_redis.core.RedisChannelLayer',
+'CONFIG': {
+"hosts": [('127.0.0.1', 6379)],
+},
+},
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
