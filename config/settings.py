9523890cf7fee4e87c90e7eb4dc54210bf5fbe10
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2025-01-21 16:27:01 +0330

    chore: add installed apps to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'kor',
+    'drf_yasg',
     'channels',
 ]
 
