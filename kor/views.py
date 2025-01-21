718287c54f88259bb236772ea863b35e5f571b0a
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2025-01-22 01:53:29 +0330

    chore: add the class view code to kor/views.py

--- kor/views.py
+++ kor/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .utils import connect_mt5, get_eurusd_data, detect_flag_pattern
 
-# Create your views here.
+class FlagDetectionView(APIView):
+    def get(self, request, *args, **kwargs):
+        connect_mt5()
+        data = get_eurusd_data()
+        result = detect_flag_pattern(data)
+        if result:
+            return Response({"flag_detected": True, "details": result})
+        return Response({"flag_detected": False, "details": None})
